01c3c8be6a9054ba31a555e3aedbdaae1b5185b7
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2021-08-28 08:29:38 -0300

    removendo codigo que não será utilizado

--- atividade_assosiacao/bin/atividade_assosiacao.dart
+++
@@ -1,5 +0,0 @@
-import 'package:atividade_assosiacao/atividade_assosiacao.dart' as atividade_assosiacao;
-
-void main(List<String> arguments) {
-  print('Hello world: ${atividade_assosiacao.calculate()}!');
-}
--- atividade_assosiacao/test/atividade_assosiacao_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:atividade_assosiacao/atividade_assosiacao.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
